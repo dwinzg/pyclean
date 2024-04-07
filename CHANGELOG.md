a9534199e31fa5e0c6166e79d6e2c61a40773369
dwinzg <75581482+dwinzg@users.noreply.github.com>
2024-04-06 19:29:24 -0700

    removed everything

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# Changelog
-
-<!--next-version-placeholder-->
-
-## v0.1.0 (06/04/2024)
-
-- First release of `pyclean`!
--- pyclean/pyclean.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-def remove_missing_values(df, axis=0):
-    """Remove rows or columns with missing values (NaNs)."""
-    return df.dropna(axis=axis)
-
-def remove_duplicates(df):
-    """Remove duplicate rows from DataFrame."""
-    return df.drop_duplicates(keep='first')
